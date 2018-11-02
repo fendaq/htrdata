2ba3dec5228ad078d9f01ba8f47bb9b2e47f1e9e
Ronny <wrhuang@mit.edu>
2018-11-02 10:09:33 -0400

    added the rest of stuff like worksheet

--- src/utils_augment.py
+++ src/utils_augment.py
@@ -1,32 +1,58 @@
+from PIL import Image
 import numpy as np
 import cv2
+from functools import reduce
 from os.path import basename, join
 import os
 import sys
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, imshow, show, colorbar
 
-def put_patch(im, patchPath, centroid):
-  '''
-  Takes patch image located in patchPath and superimpose on image (im) at centroid. Returns modified image.
-  :param im: numpy matrix of greyscale image on which to impose patch
-  :param patchPath: path of the patch image (underline, box, spurious text, etc
-  :param centroid: centroid of the numpy matrix of image on which to align centroid of patch
-  :return: modified (patched) image
-  '''
+def remove_background(im, threshold):
+  mask = im < threshold
+  imMasked = im.copy()
+  imMasked[mask] = 0
+  return imMasked
 
-  return imMod
+def merge_patch(imBase, imPatch, centroid, threshold=100):
+  '''Takes imPatch and superimpose on imBase at centroid. Returns modified image'''
+
+  imBase, imPatch = 255-imBase, 255-imPatch # invert images fro processing
+  nrb, ncb = imBase.shape
+  nrp, ncp = imPatch.shape
+
+  # make white areas of imPatch transparent
+  imPatchMasked = remove_background(imPatch, threshold)
+
+  # get difference of centroids between base and patch
+  centroidPatch = np.array([int(dim/2) for dim in imPatchMasked.shape])
+  delta = np.array(centroid) - centroidPatch
+
+  # add difference of centroids to the x,y position of patch
+  cc, rr = np.meshgrid(np.arange(ncp), np.arange(nrp))
+  rr = rr + delta[0]
+  cc = cc + delta[1]
+
+  # remove all parts of patch image that would expand base image
+  keep = reduce(np.logical_and, [rr>=0, rr<nrb, cc>=0, cc<ncb])
+  nrk, nck = np.max(rr[keep])-np.min(rr[keep])+1, np.max(cc[keep])-np.min(cc[keep])+1
+  imPatchKeep = imPatchMasked[keep]
 
 
-def superimpose_image(imBase, imArt, expand=False):
-  '''
-  Superimpose artifacts from imArt onto imBase
-  :param imBase: master image on which imArt is imposed. whitespace on this image are kept
-  :param imArt: image to superimpose. whitespace on this image (pixels below threshold) rendered transparent
-  :param expand: True if allow resizing of imMaster
-  :return: modified image
-  '''
+  # merge base and patch by taking maximum pixel at each position
+  imMerge = imBase.copy()
+  imBaseCrop = imBase.copy()
+  imBaseCrop = imBaseCrop[rr[keep], cc[keep]]
+  imMerge[rr[keep], cc[keep]] = np.maximum(imBaseCrop, imPatchKeep)
 
-  return imMasterMod
+  return 255-imMerge # invert back
 
+file = '/Users/dl367ny/htrdata/crowdsource/extracted/111003/$9,900,000.jpg'
+patchFile = '/Users/dl367ny/htrdata/cropped_patches/nw_horizontal-2/Declined - Handwritten (1)_Redacted-2-aligned-Unnamed2.jpg'
+imBase = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+im = cv2.imread(patchFile, cv2.IMREAD_GRAYSCALE)
+im = cv2.resize(im, None, fx=1, fy=1)
 
-
+nrb, ncb = imBase.shape
+centroid = int(.1*nrb), int(ncb/2)
+imMerge = merge_patch(imBase, im, centroid, 0)
+Image.fromarray(imMerge).show()
